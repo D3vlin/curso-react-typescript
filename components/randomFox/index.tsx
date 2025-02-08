5ebe9180694ab89cd94fa6f5130d14e379e125de
D3vlin <alexisdq16@gmail.com>
2025-02-08 10:02:46 -0500

    lazy loading

--- components/randomFox/index.tsx
+++ components/randomFox/index.tsx
@@ -1,4 +1,4 @@
-import { JSX } from "react"
+import { JSX, useEffect, useRef, useState } from "react"
 // import { FC, FunctionComponent, JSX } from "react"
 
 // export const RandomFox1 = () => {
@@ -7,7 +7,28 @@ import { JSX } from "react"
 type Props = { image: string }
 
 export const RandomFox = ({ image }: Props): JSX.Element => { // -> Más recomendada hoy en día
-    return <img src={image} width={320} height="auto" className="rounded-lg" />
+    const node = useRef<HTMLImageElement>(null)
+
+    const [src, setSrc] = useState("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=")
+    
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    console.log("Rendering... ", entry.target);
+                    setSrc(image)
+                }
+            })
+        })
+    
+        if (node.current) observer.observe(node.current)
+
+        return () => {
+            observer.disconnect()
+        }
+    }, [image])
+
+    return <img ref={node} src={src} width={320} height="auto" className="rounded-lg bg-gray-300" />
 }
 
 // export const RandomFox3: FunctionComponent = () => {
