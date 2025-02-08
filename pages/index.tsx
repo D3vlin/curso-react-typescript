d626bd8c02bfd7adc7fbeabc98ab850d8c839447
D3vlin <alexisdq16@gmail.com>
2025-02-08 09:37:49 -0500

    types from react

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { RandomFox } from "@/components/randomFox";
 import { NextPage } from "next";
 import Head from "next/head";
-import { useState } from "react";
+import { MouseEventHandler, useState } from "react";
 
 const random = () => Math.floor(Math.random() * 123) + 1
 
@@ -10,11 +10,13 @@ type ImageItem = { id: string, url: string}
 const generateId = () => Math.random().toString(36).substring(2, 9)
 
 export default function Home(): NextPage {
-  const [images, setImages] = useState<ImageItem[]>([
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg`},
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg`},
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg`},
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg`}])
+  const [images, setImages] = useState<ImageItem[]>([])
+
+    const addNewFox: MouseEventHandler<HTMLButtonElement> = (event) => {
+      event.preventDefault()
+      const newImageItem: ImageItem = { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg`}
+      setImages([...images, newImageItem])
+    }
 
   return (
     <div>      
@@ -23,6 +25,7 @@ export default function Home(): NextPage {
       </Head>
       <main>
         <h1 className="text-3xl font-bold underline">Hello!!</h1>
+        <button onClick={addNewFox}>Add new Fox</button>
         {
           images.map((image, index) => (
             <div key={index} className="p-4">
